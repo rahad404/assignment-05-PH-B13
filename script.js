f3f0ef6db72c1f4b41dc118ca6eb13f1c63262a2
rahad404 <rahad.0911@gmail.com>
2026-03-09 17:31:56 +0600

    add login authentication function[js]

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const loginContainer = document.getElementById('login-container');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const loginButton = document.getElementById('login-btn');
+const loginErrorMsg = document.getElementById('login-error-msg');
 
 // navigation search variables
+const navigationBar =document.getElementById('navigation-bar');
 const search = document.getElementById('search');
 const searchBtn = document.getElementById('search-btn');
 
 // main container variables
+const mainContainer = document.getElementById('main-container');
 // tab buttons variables
 const allTabBtn = document.getElementById('all-tab-btn');
 const OpenTabBtn = document.getElementById('open-tab-btn');
@@ -29,3 +32,25 @@ let closedIssues = [];
 let currentTab = 'all';
 let currentSearchText = '';
 
+// default credentials
+const defaultUsername = 'admin';
+const defaultPassword = 'admin123';
+
+// event listeners
+loginButton.addEventListener('click', loginAuthentication);
+
+function loginAuthentication() {
+    const enteredUsrname = username.value.trim();
+    const enteredPassword = password.value.trim();
+    if(enteredPassword === defaultPassword && enteredUsrname === defaultUsername){
+        alert('Login successful!');
+        loginContainer.classList.add('hidden');
+        navigationBar.classList.remove('hidden');
+        mainContainer.classList.remove('hidden');
+        fetchAllIssues(); // yet to implement
+    }
+    else{
+        loginErrorMsg.classList.remove('hidden');
+        alert('Invalid username or password. Please try again.');
+    }
+}
